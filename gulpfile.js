fc295a4c2692b62939ed5867649cb72c8a048200
Eli Golding <egolding@qualitybath.com>
2016-01-02 20:30:49 -0500

    Removed unnecessary transformer from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,6 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir.config.js.browserify.transformers.push({ name: 'browserify-shim', options: {}});
-
 elixir(function(mix) {
     mix.sass('app.scss');
     mix.browserify('main.js');
