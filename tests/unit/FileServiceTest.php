949e73e890d957be2321dbb539f9d855baf2c282
loyvin.ramirez <loyvin.ramirez@DESKTOP-QDV0U7V>
2016-02-23 19:47:42 +0800

    Added documentation.

--- tests/unit/FileServiceTest.php
+++ tests/unit/FileServiceTest.php
@@ -2,6 +2,10 @@
 
 use App\Services\FileService;
 
+/**
+ * Unit test class for File Service.
+ *
+ */
 class FileServiceTest extends \Codeception\TestCase\Test
 {
     /**
@@ -11,6 +15,9 @@ class FileServiceTest extends \Codeception\TestCase\Test
 	
     protected $fileService;
     
+    /**
+     * Instantiate the FileService.
+     */
     protected function _before()
     {
     	$this->fileService = new FileService();
@@ -20,26 +27,57 @@ class FileServiceTest extends \Codeception\TestCase\Test
     {
     }
 
+    /**
+     * Test for the getFile method of FileService.
+     * State prefix is passed as parameter. 
+     * 
+     * Asserts not null for the file retrieved.
+     * Asserts if filename is equal to expected name.
+     */
     public function testGetFile(){
     	$file = $this->fileService->getFile('ak1');
     	$this->assertNotNull($file);
     	$this->assertEquals('test.csv', $file[0]->file_name);
     }
     
+    /**
+     * Test for the getUrl method of FileService.
+     * State prefix is passed as parameter.
+     * 
+     * Asserts not null for the url retrieved.
+     * Asserts that url is equal to expected url.
+     */
     public function testGetUrl(){
     	$url = $this->fileService->getUrl('az1');
     	$this->assertNotNull($url);
     	$this->assertEquals('www.google.com', $url[0]->url);
     }
     
+    /**
+     * Test for the isStateUpdateable method of FileService.
+     * State prefix is passed as parameter.
+     * 
+     * Asserts that state is updateable.
+     */
     public function testIsStateUpdateable(){
     	$result = $this->fileService->isStateUpdateable('az1');
     	$this->assertTrue($result);
     }
     
+    /**
+     * Test for the updateStateUrl method of FileService.
+     * State prefix and new url are passed as parameters.
+     * 
+     * Asserts that the number of updated records is one.
+     * Asserts that the updated record is found in the database.
+     */
     public function testUpdateStateUrl(){
     	$result = $this->fileService->updateStateUrl('az1', 'www.yahoo.com');
     	$this->assertEquals(1, $result);
     	$this->tester->seeInDatabase('urls', array('state_prefix' => 'az1', 'url' => 'www.yahoo.com'));
+    }
+    
+    public function testRefreshRecords(){
+    	
     }
 }
