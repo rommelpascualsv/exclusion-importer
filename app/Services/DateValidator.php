61cfce7f9bdd4362da2730c7c3ef8d15ca027312
Loyvin Ramirez <loyvin.ramirez@gmail.com>
2016-03-31 15:31:51 +0800

    Migrate veritas Repositories and Services (SVG-282).

---
+++ app/Services/DateValidator.php
@@ -0,0 +1,10 @@
+<?php namespace App\Services;
+
+class DateValidator {
+
+	public static function validateString($dateString)
+	{
+    	$date = \DateTime::createFromFormat('m/d/Y', $dateString);
+    	return ! (\DateTime::getLastErrors()['warning_count'] || \DateTime::getLastErrors()['error_count']);
+	}
+}
