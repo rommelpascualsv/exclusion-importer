8b9cdc34168f3746bfb691ff84c2ece7db1096a3
karl.chan <beng_06@yahoo.com>
2016-03-31 16:57:57 +0800

    migrated controllers and routes of veritas to exclusion-importer

---
+++ app/Http/Controllers/MICnaController.php
@@ -0,0 +1,24 @@
+<?php namespace App\Http\Controllers;
+
+use App\Repositories\MICnaRepository;
+
+class MICnaController extends BaseController
+{
+	protected $repository;
+
+	public function __construct(MICnaRepository $repository)
+	{
+		$this->repository = $repository;
+	}
+
+	public function getMICnaRecord($id)
+	{
+		$record = $this->repository->find($id);
+
+		if (! $record) {
+			abort(404);
+		}
+
+		return response()->json($record);
+	}
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,13 @@ $app->get('/', function () use ($app) {
     return view('hello');
 });
 
+# exclusion_importer routes
 $app->get('import/createOldTables', 'ImportController@createOldTables');
 $app->get('import/{listPrefix}', 'ImportController@import');
 $app->get('import', 'ImportController@index');
+
+# veritas routes
+$app->get('nppes/{id}', 'App\Http\Controllers\NppesController@getNppesRecord');
+$app->get('njna/{id}', 'App\Http\Controllers\NjnaController@getNjnaRecord');
+$app->get('nj-credential/{id}', 'App\Http\Controllers\NJCredentialController@getNJCredentialRecord');
+$app->get('mi-cna/{id}', 'App\Http\Controllers\MICnaController@getMICnaRecord');
