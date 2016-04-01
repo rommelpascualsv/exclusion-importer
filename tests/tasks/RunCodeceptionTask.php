29fcbfe04da9e90657a7c8c3259381d124369eb0
karl.chan <beng_06@yahoo.com>
2016-04-01 21:29:53 +0800

    code cleanup. added EOL and fixed curly braces format

--- tests/tasks/RunCodeceptionTask.php
+++ tests/tasks/RunCodeceptionTask.php
@@ -1,23 +1,20 @@
 <?php
-class RunCodeceptionTask extends Task {
-	
+class RunCodeceptionTask extends Task 
+{
 	protected $type = null;
 	
-	public function init() {
+	public function main() 
+	{
+		$outputAndErrors = "";
+		$return_value = "";
 		
-	}
-	
-	public function main() {
-		
-		$outputAndErrors;
-		$return_value;
-		
-		exec("php codecept.phar run ".$type, $outputAndErrors, $return_value);
+		exec("php codecept.phar run " . $type, $outputAndErrors, $return_value);
 		
 		print_r(implode("\n", $outputAndErrors));
 	}
 	
-	public function setType($type) {
+	public function setType($type) 
+	{
 		$this->type = $type;
 	}
 }
