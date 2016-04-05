9b68e7a1d6cb32766f240879cae1cce329cabfee
karl.chan <beng_06@yahoo.com>
2016-04-05 17:39:40 +0800

    added common factories(test data) for veritas tests

---
+++ tests/factories/taxonomy/record.php
@@ -0,0 +1,10 @@
+<?php
+
+ArrayFactory::define('TaxonomyRecord', [
+	'code' => '101Y00000X',
+	'type' => 'Behavioral Health & Social Service Providers',
+	'classification' => 'Counselor',
+	'specialization' => '',
+	'definition' => 'A provider who is trained and educated in the performance of behavior health services through interpersonal communications and analysis. Training and education at the specialty level usually requires a master’s degree and clinical experience and supervision for licensure or certification.',
+	'notes' => 'Sources: Abridged from definitions provided by the National Board of Certified Counselors and the American Association of Pastoral Counselors.'
+]);
