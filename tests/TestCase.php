408569638c37f3c12c8785bb93678bba029794d4
Loyvin Ramirez <loyvin.ramirez@gmail.com>
2016-04-05 21:03:27 +0800

    Add requiring of factories/*.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,14 +1,23 @@
 <?php
 
-class TestCase extends Laravel\Lumen\Testing\TestCase
-{
-    /**
-     * Creates the application.
-     *
-     * @return \Laravel\Lumen\Application
-     */
+class TestCase extends Laravel\Lumen\Testing\TestCase {
+
     public function createApplication()
     {
         return require __DIR__.'/../bootstrap/app.php';
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        require __DIR__ . '/factories/nppes/record.php';
+        require __DIR__ . '/factories/nppes/row.php';
+        require __DIR__ . '/factories/nppes/deactivation_record.php';
+        require __DIR__ . '/factories/nppes/deactivation_row.php';
+        require __DIR__ . '/factories/taxonomy/row.php';
+        require __DIR__ . '/factories/taxonomy/record.php';
+        require __DIR__ . '/factories/njna/row.php';
+        require __DIR__ . '/factories/njna/record.php';
     }
 }
