6b3b61e77e1646ed43ecf5d1be11ca46e9916eea
Loyvin Ramirez <loyvin.ramirez@gmail.com>
2016-04-07 23:43:59 +0800

    Migrated some more tests from veritas.

---
+++ tests/unit/NppesDeactivateTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Repositories\NppesRepository;
+
+class NppesDeactivateTest extends MongoTestCase {
+
+	public function test_nppes_record_sets_deactiviation_date_in_database()
+	{
+		$repository = $this->app->make(NppesRepository::class);
+
+		// Get Nppes and enter it into the database
+		$nppesRecord = ArrayFactory::create('NppesRecord');
+		$repository->create($nppesRecord);
+
+		// Get Nppes deactiviation and update the database with it
+		$deactivationRecord = ArrayFactory::create('NppesDeactivationRecord');
+		$repository->update($deactivationRecord);
+
+		// Retrieve original Nppes from the database
+		$actual = $repository->find($nppesRecord['npi']);
+
+		$expected = ArrayFactory::create('NppesRecord', ['deactivation_date' => '10/10/10']);
+
+		$this->assertEquals($expected, $actual);
+	}
+}
