d250972934b81eac8d573140e2ed8d98183d48fe
karl.chan <beng_06@yahoo.com>
2016-04-12 18:56:13 +0800

    Use tabula for parsing the Alaska state

--- app/Import/Lists/Alaska.php
+++ app/Import/Lists/Alaska.php
@@ -4,9 +4,11 @@ class Alaska extends ExclusionList
 {
     public $dbPrefix = 'ak1';
 
-    public $uri = "https://s3.amazonaws.com/StreamlineVerify-Storage/exclusion-lists/alaska/aklist.xlsx";
+    public $pdfToText = "java -jar ../etc/tabula.jar -p 2-7 -u -g -r";
 
-    public $type = 'xlsx';
+    public $uri = "http://dhss.alaska.gov/Commissioner/Documents/PDF/AlaskaExcludedProviderList.pdf";
+    
+    public $type = 'pdf';
 
     public $fieldNames = [
         'exclusion_date',
@@ -35,4 +37,109 @@ class Alaska extends ExclusionList
         'exclusion_date' => 0
     ];
 
+    /**
+     * @var institution special cases
+     */
+    private $institutions = [
+    	"ANCHORAGE ADULT DAY SERVICES",
+    	"EBEN-EZER HOMECARE, LLC"
+    ];
+    
+    public function preProcess()
+    {
+    	$this->parse();
+    	parent::preProcess();
+    }
+    
+    public function parse()
+    {
+    	$rows = preg_split('/(\r)?\n(\s+)?/', $this->data);
+    	
+    	$data = [];
+    	foreach ($rows as $key => $value) {
+    		 
+    		// do not include if row is empty
+    		if (empty($value) || $this->isHeader($value)) {
+    			continue;
+    		}
+    		
+    		// convert string row to comma-delimited array
+    		$columns = str_getcsv($value);
+    		
+    		// applies specific overrides
+    		$columns = $this->applyOverrides($columns);
+    		
+    		// populate the array data
+    		array_push($data, $columns);
+    	}
+    
+    	$this->data = $data;
+    }
+    
+    /**
+     * Applies the specific overrides to correct the data
+     * @param array $columns the column array
+     * @return array $columns the column array
+     */
+    private function applyOverrides($columns)
+    {
+    	$columns = $this->addMissingColumn($columns);
+    	$columns = $this->populateFirstMiddleName($columns);
+    	
+    	return $columns;
+    }
+    
+    /**
+     * Adds and removes the columns for specific institutions
+     * @param array $columns the column array
+     * @return array $columns the column array
+     */
+    private function addMissingColumn($columns)
+    {
+    	
+    	if (in_array($columns[1], $this->institutions)) {
+    		
+    		// remove excess column
+    		array_pop($columns);
+    		
+    		// insert missing column
+    		array_splice($columns, 2, 0, "");
+    	}
+    	
+    	return $columns;
+    }
+    
+    /**
+     * Populates the first and middle name columns
+     * @param array $columns the column array
+     * @return array $columns the column array
+     */
+    private function populateFirstMiddleName($columns)
+    {
+    	$firstMiddle = explode(" ", $columns[2], 2);
+    	 
+    	if (count($firstMiddle) == 1) {
+    		$firstMiddle[1] = "";
+    	}
+    	 
+    	array_splice($columns, 2, 1, $firstMiddle);
+    	 
+    	return $columns;
+    }
+    
+    /**
+     * Determines if the value is a header.
+     * @param string $value the string row value
+     * @return boolean returns true if value is header, otherwise false.
+     */
+    private function isHeader($value)
+    {
+    	
+    	$value = str_replace("\r", "", $value);
+    	if ('"EXCLUSION DATE",LAST NAME,FIRST NAME,PROVIDER TYPE,"EXCLUSION AUTHORITY",EXCLUSION REASON' === $value) {
+    		return true;
+    	}
+    	
+    	return false;
+    }
 }
