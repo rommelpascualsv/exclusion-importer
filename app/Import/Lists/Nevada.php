c2e40b4b6be33696d63a40c13c0882e8b8f2a4af
karl.chan <beng_06@yahoo.com>
2016-04-27 13:15:42 +0800

    Initial implementation of Nevada. Only missing is how to handle multiple NPIs

---
+++ app/Import/Lists/Nevada.php
@@ -0,0 +1,165 @@
+<?php namespace App\Import\Lists;
+
+class Nevada extends ExclusionList
+{
+    public $dbPrefix = 'nv1';
+
+    public $pdfToText = "java -jar ../etc/tabula.jar -p all -c 126,220,305,350,400,440,495,532,580,635,680";
+
+    public $uri = "http://dhcfp.nv.gov/uploadedFiles/dhcfpnvgov/content/Providers/PI/NevadaProviderExclusions.pdf";
+    
+    public $type = 'pdf';
+
+    public $fieldNames = [
+    	'doing_business_as',
+    	'legal_entity',
+    	'ownership_of_at_least_5_percent',
+    	'medicaid_provider',
+    	'npi',
+    	'provider_type',	
+    	'termination_date',
+    	'sanction_tier',
+    	'sanction_period',
+    	'sanction_period_end_date',
+    	'reinstatement_date'
+    ];
+
+
+    public $retrieveOptions = [
+        'headerRow' => 0,
+        'offset'    => 1
+    ];
+
+    
+    public $hashColumns = [
+    	'doing_business_as',
+    	'legal_entity', 
+    	'npi',
+    	'termination_date',
+    	'sanction_period',
+    	'sanction_period_end_date',
+    	'reinstatement_date'
+    ];
+
+    public $dateColumns = [
+    	'termination_date' => 6,
+    	'sanction_period_end_date' => 9,
+    	'reinstatement_date' => 10
+    ];
+
+    /**
+     * @var contains the headers of the pdf that should be excluded
+     */
+    private $headers = [
+    	',,,,,,,,,,Mar 2016',
+    	',,,,,,,,,"NV ",',
+    	',,,,,,,,,"Medicaid ",',
+    	',,"Persons with ",,,,,,,"Sanction ","Federal "',
+    	',,"controlling inerest of ","Medicaid ",,"Provider ","Termination ","Sanction ","Sanction ","Period End ","Reinstate "',
+    	'Business Name,Legal Entity,5% or more,Provider,NPI,Type,Date,Tier,Period,Date,Date'
+    ];
+    
+    /**
+     * @var contains the legal entities that need to be manually corrected
+     */
+    private $legalEntity = [
+    	'Helping Others Help Themsel,ves' => 'Helping Others Help Themselves,'
+    ];
+    
+    public function preProcess()
+    {
+    	$this->parse();
+    	parent::preProcess();
+    }
+
+    public function parse()
+    {
+    	// removes all headers
+    	$this->data = str_replace($this->headers, "", $this->data);
+    	
+    	// corrects the converted data 
+    	$this->data = str_replace(array_keys($this->legalEntity), array_values($this->legalEntity), $this->data);
+    	
+    	// split into rows
+    	$rows = preg_split('/(\r)?\n(\s+)?/', $this->data);
+        
+        $data = [];
+        $mergeData = [];
+        foreach ($rows as $key => $value) {
+        	
+        	// do not include if row is empty
+        	if (empty($value)) {
+        		continue;
+        	}
+        	
+        	// convert string row to comma-delimited array
+        	$columns = str_getcsv($value);
+        	
+        	if (empty($columns[6])) {
+        		$mergeData = $this->buildMergeData($mergeData, $columns);
+        		continue;
+        	}
+        	
+        	if (!empty($mergeData)) {
+        		$columns = $this->buildColumnsData($mergeData, $columns);
+        	}
+        	
+        	// cleans the records 
+        	$columns = array_map('trim', $columns);
+        	
+			// applies specific overrides
+			$columns = $this->applyOverrides($columns);
+			
+			// populate the array data
+        	$data[] = $columns;
+        	$mergeData = [];
+        }
+
+        $this->data = $data;
+    }
+    
+    /**
+     * Builds the column records by merging the merge data
+     *
+     * @param mergeData the merge data
+     * @param columns the current column records
+     *
+     */
+    private function buildColumnsData($mergeData, $columns) {
+    	
+    	foreach ($mergeData as $k => $v) {
+    		$columns[$k] = $v . " " . $columns[$k];
+    		$columns[$k] = preg_replace('!\s+!', ' ', $columns[$k]);
+    	}
+    	return $columns;
+    }
+    
+	/** 
+	 * Builds the merge data needed for merging related rows
+	 * 
+	 * @param mergeData the merge data
+	 * @param columns the current column records
+	 * 
+	 */
+	private function buildMergeData($mergeData, $columns) {
+		
+		if (empty($mergeData)) {
+			$mergeData = $columns;
+		} else {
+			foreach ($columns as $k => $v) {
+				$mergeData[$k] = $mergeData[$k] . " " . $v;
+			}
+		}
+		return $mergeData;
+    }
+
+    /**
+     * Applies the specific overrides to correct the data
+     * @param array $columns the column array
+     * @return array $columns the column array
+     */
+    private function applyOverrides($columns)
+    {
+    	return $columns;
+    }
+}
