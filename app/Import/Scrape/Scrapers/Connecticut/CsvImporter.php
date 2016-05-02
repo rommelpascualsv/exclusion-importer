b4b18b64dcc8a0f2d44ffe9134d87ec8aa568209
Leoncio Cesar L. Polestico <leoncio.polestico.sv@gmail.com>
2016-05-02 22:20:37 +0800

    Create test implementation of importing rows from a single csv file to the db

--- app/Import/Scrape/Scrapers/Connecticut/CsvImporter.php
+++ app/Import/Scrape/Scrapers/Connecticut/CsvImporter.php
@@ -3,6 +3,8 @@
 namespace App\Import\Scrape\Scrapers\Connecticut;
 
 use Illuminate\Database\ConnectionInterface;
+use League\Csv\Reader;
+use App\Import\Scrape\Scrapers\Connecticut\Data\Mappers\MapperFactory;
 
 class CsvImporter
 {
@@ -18,14 +20,114 @@ class CsvImporter
 	
 	public function import()
 	{
+		
 	}
 	
+	/**
+	 * Extract rows from a csv file and store corresponding rosters in the db
+	 * @param array $data
+	 * @param string $timestamp
+	 */
+	public function importCsv(array $data, $timestamp)
+	{
+		$reader = Reader::createFromPath($data['file_path']);
+		$results = $reader->fetchAll();
+		$resultsCount = count($results);
+		
+		if ($resultsCount < 2) {
+			//@todo: handle no records here
+			return;
+		}
+		
+		$mapper = MapperFactory::createByKeys($data['category'], $data['option']);
+		$optionId = $this->dbFindOptionIdByKey($data['option']);
+		
+		for ($i = 1; $i < $resultsCount; $i++) {
+			$row = $results[$i];
+			$csvData = $mapper->getCsvData($row);
+			$dbData = $mapper->getDbData($csvData);
+			
+			$this->dbInsertRoster($dbData, $optionId, $timestamp);
+		}
+	}
+	
+	/**
+	 * Insert facility to database
+	 * @param string $name
+	 * @param string $timestamp
+	 */
 	public function dbInsertFacility($name, $timestamp)
 	{		
-		$this->db->table('ct_roster_facilities')->insert([
+		$id = $this->db->table('ct_roster_facilities')->insertGetId([
 				'name' => $name,
 				'created_at' => $timestamp,
 				'updated_at' => $timestamp
 		]);
+		
+		return $id;
+	}
+	
+	/**
+	 * Find facility
+	 * @param string $name
+	 * @return int|null
+	 */
+	public function dbFindFacilityIdByName($name)
+	{
+		$facility = $this->db->table('ct_roster_facilities')
+		->select('id')
+		->where('name', '=', $name)
+		->first();
+	
+		$facilityId = (is_object($facility)) ? $facility->id : null;
+	
+		return $facilityId;
+	}
+	
+	/**
+	 * Insert roster to database
+	 * @param array $data
+	 * @param int $optionId
+	 * @param string $timestamp
+	 */
+	public function dbInsertRoster(array $data, $optionId, $timestamp)
+	{
+		// get facility id
+		$facilityId = $this->dbFindFacilityIdByName($data['name']);
+		
+		if (! $facilityId) {
+			$facilityId = $this->dbInsertFacility($data['name'], $timestamp);
+		}
+		
+		$this->db->table('ct_rosters')->insert([
+				'option_id' => $optionId,
+				'facility_id' => $facilityId,
+				'address1' => $data['address1'],
+				'address2' => $data['address2'],
+				'city' => $data['city'],
+				'county' => $data['county'],
+				'state_code' => $data['state_code'],
+				'zip' => $data['zip'],
+				'complete_address' => $data['complete_address'],
+				'created_at' => $timestamp,
+				'updated_at' => $timestamp
+		]);
+	}
+	
+	/**
+	 * Find option id by key
+	 * @param string $key
+	 * @return int|null
+	 */
+	public function dbFindOptionIdByKey($key)
+	{
+		$option = $this->db->table('ct_roster_options')
+			->select('id')
+			->where('key', '=', $key)
+			->first();
+		
+		$optionId = (is_object($option)) ? $option->id : null;
+		
+		return $optionId;
 	}
 }
--- tests/unit/Import/Scrape/Scrapers/Connecticut/CsvImporterTest.php
+++ tests/unit/Import/Scrape/Scrapers/Connecticut/CsvImporterTest.php
@@ -20,12 +20,31 @@ class CsvImporterTest extends \Codeception\TestCase\Test
 	{
     	$this->db = app('db')->connection();
     	$this->importer = new CsvImporter($this->db);
+    	
+    	/* insert categories */
+    	$this->tester->haveRecord('ct_roster_categories', [
+    			'id' => 1,
+    			'key' => 'ambulatory_surgical_centers_recovery_care_centers',
+    			'name' => 'Ambulatory Surgical Centers/Recovery Care Centers',
+    			'created_at' => '2016-05-01 01:30:00',
+    			'updated_at' => '2016-05-01 01:30:00'
+    	]);
+    	
+    	/* insert options */
+    	$this->tester->haveRecord('ct_roster_options', [
+    			'id' => 1,
+    			'category_id' => 1,
+    			'key' => 'ambulatory_surgical_center',
+    			'name' => 'Ambulatory Surgical Center',
+    			'created_at' => '2016-05-01 01:30:00',
+    			'updated_at' => '2016-05-01 01:30:00'
+    	]);
     }
 
     protected function _after()
     {
     }
-	
+    
     public function testDbInsertFacility()
     {
     	$name = 'SAINT FRANCIS GI ENDOSCOPY, LLC';
@@ -38,6 +57,148 @@ class CsvImporterTest extends \Codeception\TestCase\Test
     			'created_at' => '2016-05-01 01:30:00',
     			'updated_at' => '2016-05-01 01:30:00' 
     	]);
+    }
+    
+    public function testDbFindFacilityIdByName()
+    {
+    	$this->tester->haveRecord('ct_roster_facilities', [
+    			'id' => 3,
+    			'name' => 'Some Roster'
+    	]);
+    	
+    	$name = 'Some Roster';
+    	 
+    	$facilityId = $this->importer->dbFindFacilityIdByName($name);
+    	 
+    	$this->assertEquals(3, $facilityId);
+    }
+    
+    public function testDbFindFacilityIdByNameNotFound()
+    {    	
+    	$name = 'Some Unknown Roster';
+    	 
+    	$facilityId = $this->importer->dbFindFacilityIdByName($name);
+    	 
+    	$this->assertNull($facilityId);
+    }
+    
+    public function testDbInsertRoster()
+    {
+    	$data = [
+	    	'name' => 'SAINT FRANCIS GI ENDOSCOPY, LLC',
+	    	'address1' => '360 BLOOMFIELD AVE STE 204',
+	    	'address2' => '',
+	    	'city' => 'WINDSOR',
+	    	'county' => '',
+	    	'state_code' => 'CT',
+	    	'zip' => '06095-2700',
+	    	'complete_address' => ''
+    	];
+    	$optionId = 1;
+    	$timestamp = '2016-05-01 01:30:00';
+    	
+    	$this->importer->dbInsertRoster($data, $optionId, $timestamp);
+    	
+    	$this->tester->seeRecord('ct_rosters', [
+    			'option_id' => 1,
+    			'address1' => '360 BLOOMFIELD AVE STE 204',
+		    	'address2' => '',
+		    	'city' => 'WINDSOR',
+		    	'county' => '',
+		    	'state_code' => 'CT',
+		    	'zip' => '06095-2700',
+		    	'complete_address' => ''
+    	]);
+    }
+    
+    public function testDbInsertRosterWithExistingFacility()
+    {
+    	// insert existing facility
+    	$this->tester->haveRecord('ct_roster_facilities', [
+    			'id' => 23,
+    			'name' => 'SAINT FRANCIS GI ENDOSCOPY, LLC',
+    	]);
+    	
+    	$data = [
+    			'name' => 'SAINT FRANCIS GI ENDOSCOPY, LLC',
+    			'address1' => '360 BLOOMFIELD AVE STE 204',
+    			'address2' => '',
+    			'city' => 'WINDSOR',
+    			'county' => '',
+    			'state_code' => 'CT',
+    			'zip' => '06095-2700',
+    			'complete_address' => ''
+    	];
+    	$optionId = 1;
+    	$timestamp = '2016-05-01 01:30:00';
+    	 
+    	$this->importer->dbInsertRoster($data, $optionId, $timestamp);
+    	 
+    	$this->tester->seeRecord('ct_rosters', [
+    			'option_id' => 1,
+    			'facility_id' => 23,
+    			'practitioner_id' => null,
+    			'address1' => '360 BLOOMFIELD AVE STE 204',
+    			'address2' => '',
+    			'city' => 'WINDSOR',
+    			'county' => '',
+    			'state_code' => 'CT',
+    			'zip' => '06095-2700',
+    			'complete_address' => ''
+    	]);
+    }
+    
+    public function testImportCsv()
+    {
+    	$data = [
+    			'category' => 'ambulatory_surgical_centers_recovery_care_centers',
+    			'option' => 'ambulatory_surgical_center',
+    			'file_path' => codecept_data_dir('scrape/connecticut/csv/ambulatory_surgical_centers_recovery_care_centers/ambulatory_surgical_center.csv')
+    	];
+    	$timestamp = '2016-05-01 01:30:00';
+    	
+    	$this->importer->importCsv($data, $timestamp);
+    	
+    	/* check first and last row in the csv if they are in the database */	
+    	$firstFacility = $this->tester->grabRecord('ct_roster_facilities', [
+    			'name' => 'SAINT FRANCIS GI ENDOSCOPY, LLC'
+    	]);
+    	
+    	$this->tester->seeRecord('ct_rosters', [
+    			'facility_id' => $firstFacility->id,
+    			'address1' => '360 BLOOMFIELD AVE STE 204',
+    			'city' => 'WINDSOR',
+    			'zip' => '06095-2700'
+    	]);
+    	
+    	$lastFacility = $this->tester->grabRecord('ct_roster_facilities', [
+    			'name' => 'STAMFORD ASC, LLC'
+    	]);
+    	
+    	$this->tester->seeRecord('ct_rosters', [
+    			'facility_id' => $lastFacility->id,
+    			'address1' => '200 STAMFORD PL',
+    			'city' => 'STAMFORD',
+    			'zip' => '06902-6753'
+    	]);
+    }
+    
+    public function testDbFindOptionIdByKey()
+    {
+    	$key = 'ambulatory_surgical_center';
+    	
+    	$optionId = $this->importer->dbFindOptionIdByKey($key);
+    	
+    	$this->assertEquals(1, $optionId);
+    }
+    
+    public function testDbFindOptionIdByKeyNotFound()
+    {
+    	$key = 'some_unknown_key';
+    	 
+    	$optionId = $this->importer->dbFindOptionIdByKey($key);
+    	 
+    	$this->assertNull($optionId);
     }
     
     public function testImport()
