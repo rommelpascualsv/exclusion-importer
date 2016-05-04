78d73759d64c35d8bb39b76ea66cf23e808e52cd
karl.chan <beng_06@yahoo.com>
2016-05-04 16:24:43 +0800

    support multiple npi for michigan

--- app/Import/Lists/Michigan.php
+++ app/Import/Lists/Michigan.php
@@ -28,7 +28,8 @@ class Michigan extends ExclusionList
         'sanction_source_1',
         'sanction_date_2',
         'sanction_source_2',
-        'reason'
+        'reason',
+    	'provider_number'
     ];
 
     public $hashColumns = [
@@ -46,4 +47,91 @@ class Michigan extends ExclusionList
         'sanction_date_1' => 8,
         'sanction_date_2' => 10
     ];
+    
+    public $npiColumnName = "npi_number";
+    
+    public $npiRegex = "/1\d{9}\b/";
+    
+    public $commaRegex = "/^(,+\s)?,?|(,+\s)?,?$/";
+    
+    public $spacesRegex = "!\s+!";
+    
+    /**
+     * @inherit preProcess
+     */
+    public function preProcess()
+    {
+    	$this->parse();
+    	parent::preProcess();
+    }
+    
+    /**
+     * Parse the input data
+     */
+    private function parse()
+    {
+    	$data = [];
+    	 
+    	// iterate each row
+    	foreach ($this->data as $row) {
+    		$data[] = $this->handleRow($row);
+    	}
+    	 
+    	// set back to global data
+    	$this->data = $data;
+    }
+    
+    /**
+     * Handles the data manipulation of a record array.
+     *
+     * @param array $row the array record
+     * @return array $row the array record
+     */
+    private function handleRow($row)
+    {
+    	 
+    	// set provider number
+    	$row = $this->setProviderNo($row);
+    
+    	// set npi number array
+    	$row = $this->setNpi($row);
+    	 
+    	return $row;
+    }
+    
+    /**
+     * Set the provider number by clearing the unnecessary characters
+     *
+     * @param array $row the array record
+     * @return array $row the array record
+     */
+    private function setProviderNo($row)
+    {
+    	// remove valid npi numbers
+    	$providerNo = preg_replace($this->npiRegex, "", trim($row[5]));
+    	 
+    	// remove commas
+    	$providerNo = preg_replace($this->commaRegex, "", trim($providerNo));
+    	 
+    	// remove duplicate spaces in between numbers
+    	$row[] = preg_replace($this->spacesRegex, " ", trim($providerNo));
+    	 
+    	return $row;
+    }
+    
+    /**
+     * Set the npi numbers
+     *
+     * @param array $row the array record
+     * @return array $row the array record
+     */
+    private function setNpi($row)
+    {
+    	// extract npi number/s
+    	preg_match_all($this->npiRegex, $row[5], $npi);
+    
+    	$row[5] = $npi[0];
+    	 
+    	return $row;
+    }
 }
