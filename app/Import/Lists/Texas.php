c3c97592214b4912eaaaec391d3d314022fb6784
karl.chan <beng_06@yahoo.com>
2016-05-04 21:40:41 +0800

    support multiple npi for texas

--- app/Import/Lists/Texas.php
+++ app/Import/Lists/Texas.php
@@ -46,4 +46,63 @@ class Texas extends ExclusionList
         'add_date' => 8,
         'reinstated_date' => 9,
     ];
+    
+    public $npiColumnName = "npi";
+    
+    private $npiRegex = "/1\d{9}\b/";
+    
+    /**
+     * @inherit preProcess
+     */
+    public function preProcess()
+    {
+    	$this->parse();
+    	parent::preProcess();
+    }
+    
+    /**
+     * Parse the input data
+     */
+    private function parse()
+    {
+    	$data = [];
+    
+    	// iterate each row
+    	foreach ($this->data as $row) {
+    		$data[] = $this->handleRow($row);
+    	}
+    
+    	// set back to global data
+    	$this->data = $data;
+    }
+    
+    /**
+     * Handles the data manipulation of a record array.
+     *
+     * @param array $row the array record
+     * @return array $row the array record
+     */
+    private function handleRow($row)
+    {
+    	// set npi number array
+    	$row = $this->setNpi($row);
+    
+    	return $row;
+    }
+    
+    /**
+     * Set the npi numbers
+     *
+     * @param array $row the array record
+     * @return array $row the array record
+     */
+    private function setNpi($row)
+    {
+    	// extract npi number/s
+    	preg_match_all($this->npiRegex, $row[6], $npi);
+    
+    	$row[6] = $npi[0];
+    
+    	return $row;
+    }
 }
