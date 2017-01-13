1c2e33ed63edd5d677aaaf993f8e41e2a1732735
alyzacruz <alyza.cruz@streamlineverify.com>
2017-01-13 19:10:36 +0800

    Added error handling for records in opt out list but not in NPPES collection.

---
+++ tests/unit/OptoutSeedTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Repositories\NppesRepository;
+
+class OptoutSeedTest extends \CDM\Test\MongoTestCase
+{
+    public function test_nppes_record_updates_record_database()
+    {
+        $repository = $this->app->make(NppesRepository::class);
+
+        // Get Nppes and enter it into the database
+        $nppesRecord = ArrayFactory::create('NppesRecord');
+        $repository->create($nppesRecord);
+
+        // Get Nppes with updated practice city name and update the database with it
+        $updateRecord = ArrayFactory::create('NppesRecord', ['address' => ['practice' => ['city_name' => 'CAMBRIDGE']]]);
+        $repository->createOrUpdate($updateRecord);
+
+        // Retrieve the originally entered Nppes from the database
+        $actual = $repository->find($nppesRecord['npi']);
+
+        // Ensure originally entered Nppes is equal to the updated Nppes
+        $this->assertEquals($updateRecord, $actual);
+    }
+
+    public function test_optout_seed()
+    {
+        $repository = $this->app->make(NppesRepository::class);
+
+        // Get Nppes and enter it into the database
+        $nppesRecord = ArrayFactory::create('NppesRecord');
+        $repository->create($nppesRecord);
+
+        // Get Nppes with updated practice city name and update the database with it
+        $updateRecord = ArrayFactory::create('NppesRecord', ['address' => ['practice' => ['city_name' => 'CAMBRIDGE']]]);
+        $repository->createOrUpdate($updateRecord);
+
+        // Retrieve the originally entered Nppes from the database
+        $actual = $repository->find($nppesRecord['npi']);
+
+        // Ensure originally entered Nppes is equal to the updated Nppes
+        $this->assertEquals($updateRecord, $actual);
+    }
+
+}
